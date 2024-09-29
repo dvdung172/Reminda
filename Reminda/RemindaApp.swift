d8f134cf9c10a64e48ec2cf7d8e8835401bac179
Z02899 <nguyen.viet.dung@zensho.com>
2024-09-29 21:50:39 +0700

    Initial Commit

---
+++ Reminda/RemindaApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindaApp.swift
+//  Reminda
+//
+//  Created by Dung-pc on 29/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct RemindaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
